238982d0671cb6d4bc7a2c7ea5610eea2d915ff0
rucpata <rucinska.patrycja@gmail.com>
2021-03-05 15:22:14 +0100

    handle a sorting option change (on click)

--- src/components/SearchBar/SerachBar.js
+++ src/components/SearchBar/SerachBar.js
@@ -34,7 +34,11 @@ class SearchBar extends React.Component{
     renderSortByOptions(){
         return Object.keys(this.sortByOptions).map(sortByOption => {
             let sortByOptionValue = this.sortByOptions[sortByOption];
-            return <li key={sortByOptionValue} > {sortByOption} </li>;
+            return <li className={this.getSortByClass(sortByOptionValue)} 
+                            key={sortByOptionValue}
+                            onClick = {handleSortByChange.bind(this, sortByOptionValue)}>
+                        {sortByOption}   
+                    </li>;
         });
     }
 
